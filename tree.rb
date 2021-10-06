7eb2bb242621a7b0f9376172059c196f8a93e0d5
Michael Chong <mikec0920@gmail.com>
2021-10-06 12:00:49 -0700

    Implemented build_tree

--- tree.rb
+++ tree.rb
@@ -2,11 +2,16 @@ require_relative 'node'
 
 class Tree
   def initialize(arr = [])
-    @data = arr
-    @root
+    @root = build_tree(arr.uniq!.sort)
   end
 
-  def build_tree
-
+  def build_tree(arr)
+    middle = arr.length / 2
+    root = Node.new(arr[middle])
+    left = arr[0...middle]
+    right = arr[middle..-1]
+    root.left = build_tree(left) unless left.empty?
+    root.right = build_tree(right) unless right.empty?
+    root
   end
 end
