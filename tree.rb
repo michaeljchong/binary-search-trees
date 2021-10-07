78310185e2aca53c9701ef76f2f2b56f99d5dd83
Michael Chong <mikec0920@gmail.com>
2021-10-07 15:51:20 -0700

    Refactored insert and find methods

--- tree.rb
+++ tree.rb
@@ -1,4 +1,5 @@
 require_relative 'node'
+require 'pry-byebug'
 
 class Tree
   include Comparable
@@ -17,44 +18,46 @@ class Tree
   end
 
   def insert(value, node = @root)
-    return if find(value)
+    return Node.new(value) if node.nil?
 
     if value < node.data
-      if node.left.nil?
-        node.left = Node.new(value)
-      else
-        insert(value, node.left)
-      end
+      node.left = insert(value, node.left)
     elsif value > node.data
-      if node.right.nil?
-        node.right = Node.new(value)
-      else
-        insert(value, node.right)
-      end
+      node.right = insert(value, node.right)
     end
+    node
   end
 
-  def delete(value)
+  def delete(value, node = @root)
+    # return unless find(value)
 
+    # if value < node.data
+
+    # if node.left.nil? && node.right.nil?
+    #   # delete leaf
+    # elsif node.left.nil?
+    #   node = node.right
+    #   node.right = nil
+    # elsif node.right.nil?
+    #   node = node.left
+    #   node.left = nil
+    # end
   end
 
   def find(value, node = @root)
-    return nil if node.nil?
+    return node if node.nil? || node.data == value
 
-    if value < node.data
-      find(value, node.left)
-    elsif value > node.data
-      find(value, node.right)
-    else
-      node
-    end
+    return find(value, node.left) if value < node.data
+
+    find(value, node.right)
   end
 end
 
-data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-tree = Tree.new(data)
-p tree.search(6345)
+data1 = [1, 4, 6, 8]
+data2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+tree = Tree.new(data1)
 p tree.find(6345)
-tree.insert(6)
+tree.insert(7)
 p tree.find(6)
 p tree.find(7)
+p tree
